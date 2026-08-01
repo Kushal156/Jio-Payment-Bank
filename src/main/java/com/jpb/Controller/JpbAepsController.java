773b47a5b48a0c38261615ff983ab0e139c910e2
NikhilRautt <nikhilraut1210@gmail.com>
2026-08-01 12:59:32 +0530

    NIKHIL

--- src/main/java/com/jpb/Controller/JpbAepsController.java
+++ src/main/java/com/jpb/Controller/JpbAepsController.java
@@ -1,9 +1,8 @@
 package com.jpb.Controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.jpb.DTO.AepsCommonResponseDto;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.jpb.DTO.AepsCommonRequestDto;
 import com.jpb.DTO.JpbAepsResponseDto;
@@ -13,58 +12,92 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/jpb/AEPS")
+@CrossOrigin("*")
 public class JpbAepsController {
+
 
 	@Autowired
 	private JpbAepsService jpbAepsService;
-	
+
 	@PostMapping("/cashDeposit")
-	public JpbAepsResponseDto CashDeposite(@RequestBody AepsCommonRequestDto request,HttpServletRequest httpRequest){
-		JpbAepsResponseDto response = jpbAepsService.CashDeposite(request,  httpRequest);
+	public AepsCommonResponseDto CashDeposite(@RequestBody AepsCommonRequestDto request,HttpServletRequest httpRequest){
+
+
+
+		AepsCommonResponseDto	response =jpbAepsService.CashDeposite(request,  httpRequest);
+
 		return response;
 	}
-	
+
+	@PostMapping("/authenticate")
+	public JpbAepsResponseDto aepsAuthenticate(@RequestBody AepsCommonRequestDto request,
+											   HttpSession session,
+											   HttpServletRequest httpRequest) throws Exception{
+
+		JpbAepsResponseDto	response =jpbAepsService.eAuthenticate(request,  session, httpRequest);
+
+		return response;
+	}
+
+
 	@PostMapping("/aepscashwithdrawal")
-	public JpbAepsResponseDto CashWithdrawal(@RequestBody AepsCommonRequestDto request,HttpServletRequest httpRequest){
-		JpbAepsResponseDto	response = jpbAepsService.AepsCashWithdrawal(request,  httpRequest);
+	public AepsCommonResponseDto CashWithdrawal(@RequestBody AepsCommonRequestDto request,HttpServletRequest httpRequest){
+
+		System.out.println("request>>>"+request);
+		AepsCommonResponseDto	response =jpbAepsService.AepsCashWithdrawal(request,  httpRequest);
+
 		return response;
 	}
-	
+
 	@PostMapping("/aepsFundTransfer")
 	public JpbAepsResponseDto fundTransfer(@RequestBody AepsCommonRequestDto request,HttpServletRequest httpRequest) {
-		JpbAepsResponseDto response = jpbAepsService.AepsFundTransfer(request,httpRequest);
-		return response;	  
+
+		JpbAepsResponseDto response =	jpbAepsService.AepsFundTransfer(request,httpRequest);
+
+		return response;
 	}
-	
+
 	@PostMapping("/aepsMiniStatement")
-	public JpbAepsResponseDto miniStatement(@RequestBody AepsCommonRequestDto request,HttpServletRequest httpRequest ) {     
-		JpbAepsResponseDto response = jpbAepsService.AepsMiniStement(request,httpRequest);
+	public AepsCommonResponseDto miniStatement(@RequestBody AepsCommonRequestDto request, HttpServletRequest httpRequest ) {
+
+		AepsCommonResponseDto response =jpbAepsService.AepsMiniStement(request,httpRequest);
+
 		return response;
-	}	
-	
-	@PostMapping("/balanceInquiry")
-	public JpbAepsResponseDto BalanceInquiry(@RequestBody AepsCommonRequestDto request, HttpServletRequest httpRequest) {
-		JpbAepsResponseDto response = jpbAepsService.BalanceInquiry(request,httpRequest);
-		return response;
-		
 	}
-	
+
+	@PostMapping("/balanceInquiry")
+	public AepsCommonResponseDto BalanceInquiry(@RequestBody AepsCommonRequestDto request, HttpServletRequest httpRequest) {
+
+		AepsCommonResponseDto response =	jpbAepsService.BalanceInquiry(request,httpRequest);
+
+		return response;
+
+	}
+
 	@PostMapping("/agentHistory")
 	public JpbAepsResponseDto agentHistory(@RequestBody AepsCommonRequestDto request,HttpServletRequest httpRequest) {
+
 		JpbAepsResponseDto response = jpbAepsService.AgentHistory(request,httpRequest);
+
 		return response;
 	}
-	
+
 	@PostMapping("/agentInfo")
-	public JpbAepsResponseDto agentInfo(@RequestBody AepsCommonRequestDto request,HttpServletRequest httpRequest) {
-		JpbAepsResponseDto response = jpbAepsService.AgentInfo(request, httpRequest);
+	public JpbAepsResponseDto agentInfo(@RequestBody AepsCommonRequestDto request,
+										HttpSession session
+			,HttpServletRequest httpRequest) {
+
+		JpbAepsResponseDto response =jpbAepsService.AgentInfo(request, session,httpRequest);
+
 		return response;
 	}
-	
-	@PostMapping("/cashDepositegenerateotp")
+
+	@PostMapping("/cashWithdraGenerateOtp")
 	public JpbAepsResponseDto cashWithdrawalGenerateOtp(@RequestBody AepsCommonRequestDto request,HttpServletRequest httpRequest) {
-		JpbAepsResponseDto response = jpbAepsService.CashdepositeGenerateOtp(request, httpRequest);
+
+		JpbAepsResponseDto response =jpbAepsService.CashdepositeGenerateOtp(request, httpRequest);
+
 		return response;
 	}
-	
+
 }
