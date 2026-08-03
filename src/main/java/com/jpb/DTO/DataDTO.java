5fd153d600c76bcbec3719725964c2736a4b2cb9
NikhilRautt <nikhilraut1210@gmail.com>
2026-08-03 17:10:28 +0530

    changes for aeps nikhil

--- src/main/java/com/jpb/DTO/DataDTO.java
+++ src/main/java/com/jpb/DTO/DataDTO.java
@@ -24,4 +24,53 @@ public class DataDTO {
 	//DMT Register User Params
 	private String middleName;
 
+
+	//Aeps common response
+	private String terminalId;
+	private String requestTransactionTime;
+	private String transactionAmount;
+	private String transactionStatus;
+	private String balanceAmount;
+	private String miniStatementBalance;
+	private String bankRRN;
+	private String transactionType;
+	private String jioTransactionId;
+	private String merchantTxnId;
+	private String errorCode ;
+	private String errorMessage;
+	private String merchantTransactionId;
+	private String bankAccountNumber;
+	private String ifscCode;
+	private String bcName ;
+	private String transactionTime;
+	private String agentId;
+	private String issuerBank;
+	private String customerAadhaarNumber;
+	private String customerName;
+	private String stan;
+	private String rrn;
+	private String uidaiAuthCode;
+	private String bcLocation;
+	private String demandSheetId;
+	private String mobileNumber;
+	private String urnId;
+	//private String miniStatementStructureModel;
+	private String miniOffusStatementStructureModel;
+	private String miniOffusFlag;
+	private String transactionRemark;
+	private String bankName;
+	private String prospectNumber;
+	private String internalReferenceNumber;
+	private String biTxnType;
+	private String subVillageName;
+	private String virtualId;
+	private String userProfileResponseModel;
+	private String hindiErrorMessage;
+	private String loanAccNo;
+	private String responseCode;
+	private String jiokAgentId;
+	private String additionalData;
+
+	private List<MiniStatementDto> miniStatementStructureModel;
+
 }
